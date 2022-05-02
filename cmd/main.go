38207bcdbf33b1ba6f28cbfe31f107c3bd78b1dd
yura30rus <yura.30rus@yandex.ru>
2022-05-02 23:27:17 +0400

    Первая структура приложения

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/yura30rus/monopoly"
+	"github.com/yura30rus/monopoly/pkg/handler"
+	"log"
+)
+
 func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(monopoly.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 
 }
