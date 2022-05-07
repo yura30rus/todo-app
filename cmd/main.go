32957e31df6ae5b9a08161ab931871ebb1393e48
yura30rus <yura.30rus@yandex.ru>
2022-05-07 23:24:13 +0400

    "Исправлено название репозитория, а так же все зависимости"

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	todo_app "github.com/yura30rus/todo-app"
+	"github.com/yura30rus/todo-app/pkg/handler"
+	"github.com/yura30rus/todo-app/pkg/repository"
+	"github.com/yura30rus/todo-app/pkg/service"
+	"log"
+)
+
+func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handdlers := handler.NewHandler(services)
+
+	srv := new(todo_app.Server)
+	if err := srv.Run("8000", handdlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+
+}
